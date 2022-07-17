aa6b505b6f0eae5a9c37b4772a749ec6a5671227
s.claes <sebastian.claes@protonmail.com>
2022-07-17 18:46:46 +0200

    [WIP] Add asset row template column structure

--- src/components/AssetTab/AssetContainer.vue
+++ src/components/AssetTab/AssetContainer.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="asset-container">
-
+    <div class="asset-image" :class="(asset.isSelected) ? 'selected' : ''"></div>
+    <div class="asset-info">
+      <span class="asset-name">{{asset.name}}</span>
+      <div class="asset-info-row">
+        <div class="asset-type">{{asset.type}}</div>
+        <span class="isin label">ISIN</span>
+        <span class="isin">{{asset.isin}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AssetGroup',
+  name: 'AssetContainer',
+  props: [
+      'asset'
+  ],
+  methods: {
+    formatAssetShares(shares) {
+      this.asset.shares = 'Test'
+    }
+  }
 }
 </script>
