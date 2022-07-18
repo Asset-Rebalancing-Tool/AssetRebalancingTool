ebd6a3f669496a506d11d4ae938accb24b10e3f3
s.claes <sebastian.claes@protonmail.com>
2022-07-18 15:56:14 +0200

    [ADD] moved the asset container component and added its column components

---
+++ src/components/AssetTab/AssetRow/AssetContainer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="asset-container">
+    <AssetInfoColumn             :asset="asset" />
+    <AssetPriceColumn            :asset="asset" />
+    <AssetSharesColumn           :asset="asset" />
+    <AssetActualValueColumn      :asset="asset" />
+    <AssetTargetPercentageColumn :asset="asset" />
+    <AssetDeviationColumn        :asset="asset" />
+
+  </div>
+</template>
+
+<script>
+
+import AssetInfoColumn             from '@/components/AssetTab/AssetRow/AssetColumn/AssetInfoColumn';
+import AssetPriceColumn            from '@/components/AssetTab/AssetRow/AssetColumn/AssetPriceColumn';
+import AssetSharesColumn           from '@/components/AssetTab/AssetRow/AssetColumn/AssetSharesColumn';
+import AssetActualValueColumn      from '@/components/AssetTab/AssetRow/AssetColumn/AssetActualValueColumn';
+import AssetTargetPercentageColumn from '@/components/AssetTab/AssetRow/AssetColumn/AssetTargetPercentageColumn';
+import AssetDeviationColumn        from '@/components/AssetTab/AssetRow/AssetColumn/AssetDeviationColumn';
+
+
+export default {
+  name: 'AssetContainer',
+  components: {
+    AssetInfoColumn,
+    AssetPriceColumn,
+    AssetSharesColumn,
+    AssetActualValueColumn,
+    AssetTargetPercentageColumn,
+    AssetDeviationColumn,
+  },
+  props: [
+      'asset'
+  ],
+}
+</script>
