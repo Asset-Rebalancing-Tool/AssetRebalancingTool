823b5c288971f58d8d8acfd080afbcddc624250d
s.claes <sebastian.claes@protonmail.com>
2022-07-18 15:47:29 +0200

    [NEW] created asset row column component for the asset shares input

---
+++ src/components/AssetTab/AssetRow/AssetColumn/AssetSharesColumn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="shares-wrapper">
+    <input type="text" :value="asset.shares"/>
+    <label>{{asset.sharesType}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetSharesColumn',
+  props: [
+      'asset'
+  ]
+}
+</script>
