c47d6fb5e14a2a529d110276dec9db049bf15b75
s.claes <sebastian.claes@protonmail.com>
2022-07-18 17:59:09 +0200

    [ADD] created store action, to deselect all assets and then select an asset that is getting handed over from the AssetTab.vue component

--- src/stores/AssetStore.js
+++ src/stores/AssetStore.js
@@ -8,4 +8,15 @@ export const useAssetStore = defineStore('AssetStore', {
     state: () => {
         return { assets }
     },
+
+    actions: {
+        // Select an asset after deselect all other assets
+        selectedAsset(thisAsset) {
+            const keys = Object.keys(this.assets);
+            keys.forEach((key) => {
+                this.assets[key].isSelected = false
+            });
+            thisAsset.isSelected = true
+        }
+    }
 })
