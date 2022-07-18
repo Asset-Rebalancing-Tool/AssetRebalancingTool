d62c4e93f89675fb68b153f261fd7c94884fd30d
s.claes <sebastian.claes@protonmail.com>
2022-07-18 21:42:04 +0200

    [ADD] actionButtonsAreActive state and an action to check the state. This state will activate or deactivate the action buttons

--- src/stores/AssetStore.js
+++ src/stores/AssetStore.js
@@ -6,7 +6,10 @@ import assets from '@/data/assets.json'
 export const useAssetStore = defineStore('AssetStore', {
 
     state: () => {
-        return { assets }
+        return {
+            assets,
+            actionButtonsAreActive: false
+        }
     },
 
     actions: {
@@ -14,8 +17,22 @@ export const useAssetStore = defineStore('AssetStore', {
         // Toggle the is selected flag of an asset
         // This flag indicates if an asset can be edited, moved, or deleted by the ActionButtons
         // It also causes a visual highlighting of the asset
+        // TODO: use other loop with break option, for performance sake (other loops won't allow this.assets[key] why?)
         toggleIsSelectedFlag(thisAsset) {
+            // Toggle the isSelected flag of thisAsset
             thisAsset.isSelected = (!thisAsset.isSelected);
+            this.checkActionButtonsFlag()
+        },
+
+        // Check if at least one asset has the isSelected flag
+        checkActionButtonsFlag() {
+            // Always reset the actionButtonsAreActive flag
+            this.actionButtonsAreActive = false;
+            // Iterate over each asset and set the actionButtonsAreActive flag to true if at least on asset is selected
+            const keys = Object.keys(this.assets);
+            keys.forEach((key) => {
+                if (this.assets[key].isSelected) this.actionButtonsAreActive = true
+            });
         },
 
         // Set the is selected flag of all assets to false
@@ -25,6 +42,7 @@ export const useAssetStore = defineStore('AssetStore', {
             keys.forEach((key) => {
                 this.assets[key].isSelected = false
             });
+            this.actionButtonsAreActive = false
         }
 
 
