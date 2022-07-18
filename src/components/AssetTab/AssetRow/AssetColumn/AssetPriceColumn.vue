cb61448cd4c82526e8d56aef8888cf72cadd0ab9
s.claes <sebastian.claes@protonmail.com>
2022-07-18 15:46:03 +0200

    [NEW] created asset row column component for the asset price

---
+++ src/components/AssetTab/AssetRow/AssetColumn/AssetPriceColumn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="asset-price-wrapper">
+    <span class="primary-asset-price">{{asset.formattedStockPrice[0]}}</span>
+    <span class="secondary-price-wrapper">
+         <span class="decimal-wrapper">
+           <span class="secondary-asset-price">{{asset.formattedStockPrice[1]}}</span>
+           <span class="tertiary-asset-price">{{asset.formattedStockPrice[2]}}</span>
+         </span>
+         <span class="asset-price-currency">{{asset.currency}}</span>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetPriceColumn',
+  props: [
+      'asset'
+  ]
+}
+</script>
