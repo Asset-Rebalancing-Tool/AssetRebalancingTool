b04cebc7e30386301b4909821eee10ccf8af67c0
s.claes <sebastian.claes@protonmail.com>
2022-07-18 15:49:37 +0200

    [NEW] created asset row column component for the asset deviation

---
+++ src/components/AssetTab/AssetRow/AssetColumn/AssetDeviationColumn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="asset-deviation-wrapper">
+    <span class="primary-asset-deviation">{{asset.formattedDeviation[0]}}</span>
+    <span class="secondary-deviation-wrapper">
+         <span class="decimal-wrapper">
+           <span class="secondary-asset-deviation">{{asset.formattedDeviation[1]}}</span>
+           <span class="tertiary-asset-deviation">{{asset.formattedDeviation[2]}}</span>
+         </span>
+         <span class="asset-deviation-currency">{{asset.currency}}</span>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetDeviationColumn',
+  props: [
+      'asset'
+  ]
+}
+</script>
