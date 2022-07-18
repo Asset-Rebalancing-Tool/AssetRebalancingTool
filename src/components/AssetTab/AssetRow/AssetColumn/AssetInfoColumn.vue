d247059752b2167e05f74c331489f2d8255c6fe8
s.claes <sebastian.claes@protonmail.com>
2022-07-18 15:45:31 +0200

    [NEW] created asset row column component for the asset information like image, name, type and isin

---
+++ src/components/AssetTab/AssetRow/AssetColumn/AssetInfoColumn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="asset-info-wrapper">
+    <div class="asset-image" :class="(asset.isSelected) ? 'selected' : ''"></div>
+    <div class="asset-name-wrapper">
+      <span class="asset-name">{{asset.name}}</span>
+      <div class="asset-info-row">
+        <div class="asset-type">{{asset.type}}</div>
+        <span class="isin label">ISIN</span>
+        <span class="isin">{{asset.isin}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetInfoColumn',
+  props: [
+      'asset'
+  ]
+}
+</script>
