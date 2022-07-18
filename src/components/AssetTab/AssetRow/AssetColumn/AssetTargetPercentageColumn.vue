b3a6b4bbd4f06ba842185216f564eaed034bb1f3
s.claes <sebastian.claes@protonmail.com>
2022-07-18 15:48:51 +0200

    [NEW] created asset row column component for the target percentage of the asset

---
+++ src/components/AssetTab/AssetRow/AssetColumn/AssetTargetPercentageColumn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="target-percentage-wrapper">
+    <input type="text" :value="asset.targetPercentage"/>
+    <label>%</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetTargetPercentageColumn',
+  props: [
+      'asset'
+  ]
+}
+</script>
