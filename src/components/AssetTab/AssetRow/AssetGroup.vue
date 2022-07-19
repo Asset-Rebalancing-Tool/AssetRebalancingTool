e6721445f6943dc8dc47499d8d8b2a05c1d0258f
s.claes <sebastian.claes@protonmail.com>
2022-07-19 09:22:03 +0200

    [WIP] render the asset group template along with its AssetContainers or just the AssetContainers, based on the group key

--- src/components/AssetTab/AssetRow/AssetGroup.vue
+++ src/components/AssetTab/AssetRow/AssetGroup.vue
@@ -1,9 +1,48 @@
 <template>
 
+  <div v-if="thisGroup.groupKey !== 'NONE'">
+    <AssetContainer
+          v-if="thisGroup.groupKey !== 'NONE'"
+          v-for="asset in thisGroup.assets"
+          :key="asset.tickerSymbol"
+          :asset="asset"
+          @click="toggleIsSelectedFlag(asset)"
+    />
+    <p>{{thisGroup.name}}</p>
+  </div>
+
+  <AssetContainer
+      v-if="thisGroup.groupKey === 'NONE'"
+      v-for="asset in thisGroup.assets"
+      :key="asset.tickerSymbol"
+      :asset="asset"
+      @click="toggleIsSelectedFlag(asset)"
+  />
+
 </template>
 
 <script>
+import AssetContainer  from '@/components/AssetTab/AssetRow/AssetContainer'
+import {useAssetStore} from '@/stores/AssetStore';
+
 export default {
   name: 'AssetGroup',
+  components: {
+    AssetContainer
+  },
+  props: [
+    'thisGroup',
+  ],
+  setup() {
+    const assetStore = useAssetStore()
+    return {
+      assetStore
+    }
+  },
+  methods: {
+    toggleIsSelectedFlag(thisAsset) {
+      this.assetStore.toggleIsSelectedFlag(thisAsset);
+    }
+  }
 }
 </script>
