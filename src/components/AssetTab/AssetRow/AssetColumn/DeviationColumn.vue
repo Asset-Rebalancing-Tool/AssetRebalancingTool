4d6563edb229c6f92461c69d7258a131f449dece
s.claes <sebastian.claes@protonmail.com>
2022-07-19 11:25:00 +0200

    [ADD] removed asset prefix from asset column component name and pass single props instead of whole object

---
+++ src/components/AssetTab/AssetRow/AssetColumn/DeviationColumn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="asset-deviation-wrapper">
+    <span class="primary-asset-deviation">{{beforeDecimal}}</span>
+    <span class="secondary-deviation-wrapper">
+         <span class="decimal-wrapper">
+           <span class="secondary-asset-deviation">{{firstDecimal}}</span>
+           <span class="tertiary-asset-deviation">{{secondDecimal}}</span>
+         </span>
+         <span class="asset-deviation-unit">%</span>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DeviationColumn',
+  props: [
+      'formattedDeviation'
+  ],
+  computed: {
+    beforeDecimal() {
+      return this.formattedDeviation[0];
+    },
+    firstDecimal() {
+      return this.formattedDeviation[1];
+    },
+    secondDecimal() {
+      return this.formattedDeviation[2];
+    }
+  }
+}
+</script>
