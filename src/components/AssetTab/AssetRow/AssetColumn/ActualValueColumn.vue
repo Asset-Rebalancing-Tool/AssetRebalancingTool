14f1fbbe980fdc5b27eda6550cfeea06f4da7796
s.claes <sebastian.claes@protonmail.com>
2022-07-19 11:13:41 +0200

    [ADD] removed asset prefix from asset column component name and pass single props instead of whole object

---
+++ src/components/AssetTab/AssetRow/AssetColumn/ActualValueColumn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="actual-value-wrapper">
+    <label>{{actualValue}} {{currency}}</label>
+    <label>{{actualPercentage}} {{currency}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActualValueColumn',
+  props: [
+      'actualValue',
+      'actualPercentage',
+      'currency'
+  ],
+}
+</script>
