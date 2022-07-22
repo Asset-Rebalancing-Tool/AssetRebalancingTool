7d34d346b7ed8a156e35ade971a7d87217646e7d
s.claes <sebastian.claes@protonmail.com>
2022-07-22 18:41:43 +0200

    [REFACTOR] new more precisely comments in the AssetStore.js

--- src/stores/AssetStore.js
+++ src/stores/AssetStore.js
@@ -11,9 +11,47 @@ export const useAssetStore = defineStore('AssetStore', {
         }
     },
 
-
-
     actions: {
+
+        /**
+         * Toggle the is selected flag of an asset
+         *
+         * This flag indicates if an asset can be edited, moved, or deleted by the ActionButtons
+         * It also causes a visual highlighting of the asset
+         *
+         * TODO: use other loop with break option, for performance sake (other loops won't allow this.assets[key] why?)
+         * @param thisAsset
+         */
+        toggleIsSelectedFlag(thisAsset) {
+            // Toggle the isSelected flag of thisAsset
+            thisAsset.isSelected = (!thisAsset.isSelected);
+            this.checkActionButtonsFlag()
+        },
+
+        /**
+         * Check if at least one asset has the isSelected flag
+         */
+        checkActionButtonsFlag() {
+            // Always reset the actionButtonsAreActive flag
+            this.actionButtonsAreActive = false;
+            // Iterate over each asset and set the actionButtonsAreActive flag to true if at least on asset is selected
+            const keys = Object.keys(this.assets);
+            keys.forEach((key) => {
+                if (this.assets[key].isSelected) this.actionButtonsAreActive = true
+            });
+        },
+
+        /**
+         * Set the is selected flag of all assets to false
+         * This will also lead to a deactivation of the Actionbuttons
+         */
+        deselectAllAssets() {
+            const keys = Object.keys(this.assets);
+            keys.forEach((key) => {
+                this.assets[key].isSelected = false
+            });
+            this.actionButtonsAreActive = false
+        },
 
         getValueArray(assetValue) {
             // Parse the value of the asset to string
@@ -44,38 +82,5 @@ export const useAssetStore = defineStore('AssetStore', {
 
             return valueArray
         },
-
-        // Toggle the is selected flag of an asset
-        // This flag indicates if an asset can be edited, moved, or deleted by the ActionButtons
-        // It also causes a visual highlighting of the asset
-        // TODO: use other loop with break option, for performance sake (other loops won't allow this.assets[key] why?)
-        toggleIsSelectedFlag(thisAsset) {
-            // Toggle the isSelected flag of thisAsset
-            thisAsset.isSelected = (!thisAsset.isSelected);
-            this.checkActionButtonsFlag()
-        },
-
-        // Check if at least one asset has the isSelected flag
-        checkActionButtonsFlag() {
-            // Always reset the actionButtonsAreActive flag
-            this.actionButtonsAreActive = false;
-            // Iterate over each asset and set the actionButtonsAreActive flag to true if at least on asset is selected
-            const keys = Object.keys(this.assets);
-            keys.forEach((key) => {
-                if (this.assets[key].isSelected) this.actionButtonsAreActive = true
-            });
-        },
-
-        // Set the is selected flag of all assets to false
-        // This will also lead to a deactivation of the Actionbuttons
-        deselectAllAssets() {
-            const keys = Object.keys(this.assets);
-            keys.forEach((key) => {
-                this.assets[key].isSelected = false
-            });
-            this.actionButtonsAreActive = false
-        }
-
-
     }
 })
