2f38139a068e27d79bf7c8a92e7c4ef7dee1d4c6
s.claes <sebastian.claes@protonmail.com>
2022-07-23 13:17:56 +0200

    [NEW] created AssetFooter.vue component

---
+++ src/components/tab/asset/row/AssetFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="asset-footer">
+    <p>Gesamtsumme</p>
+    <div class="wrapper">
+      <span>20.000,00 €</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetFooter'
+}
+</script>
+
+<style scoped>
+  .asset-footer {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    background-color: var(--tertiary-background-color);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 25px;
+  }
+
+  p, span {
+    color: var(--primary-text-color);
+    font-weight: 600;
+    font-size: 18px;
+  }
+</style>
