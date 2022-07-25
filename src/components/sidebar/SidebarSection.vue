87b90c5b8e0634b044cf7d176647d49d37cf1168
s.claes <sebastian.claes@protonmail.com>
2022-07-25 13:59:12 +0200

    [ADD] stylings for the sidebar router links

--- src/components/sidebar/SidebarSection.vue
+++ src/components/sidebar/SidebarSection.vue
@@ -5,10 +5,24 @@
       <div class="app-logo" />
     </header>
 
-    <router-link class="overview" :to="{ name: 'OverviewTab' }">Übersicht</router-link> |
-    <router-link class="asset"    :to="{ name: 'AssetTab'    }">Assets verwalten</router-link>
-    <router-link class="broker"   :to="{ name: 'BrokerTab'   }">Broker Anbindung</router-link>
-    <router-link class="history"  :to="{ name: 'HistoryTab'  }">Transaktionshistorie</router-link>
+    <nav>
+      <router-link class="overview" :to="{name: 'OverviewTab'}">
+        <span class="icon overview"></span>
+        <span>Übersicht</span>
+      </router-link>
+      <router-link class="asset" :to="{name: 'AssetTab'}">
+        <span class="icon asset"></span>
+        <span>Assets verwalten</span>
+      </router-link>
+      <router-link class="broker" :to="{name: 'BrokerTab'}">
+        <span class="icon broker"></span>
+        <span>Broker Anbindung</span>
+      </router-link>
+      <router-link class="history" :to="{name: 'HistoryTab' }">
+        <span class="icon history"></span>
+        <span>Transaktionshistorie</span>
+      </router-link>
+    </nav>
 
   </section>
 </template>
@@ -46,4 +60,65 @@ export default {
     background-position: center;
     background-image: var(--app-logo);
   }
+
+  nav {
+    width: 100%;
+  }
+
+  a {
+    position: relative;
+    width: 100%;
+    height: 48px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    border: none;
+    text-decoration: none;
+    border-right: 5px solid var(--secondary-background-color);
+    background-color: transparent;
+  }
+
+  a.router-link-exact-active {
+    border-color: var(--main-color);
+  }
+
+  a.router-link-exact-active:before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 80%;
+    content: "";
+    background-image: linear-gradient(90deg, rgba(25, 179, 153, 0), rgba(25, 179, 153, 0.24));
+  }
+
+  .icon {
+    width: 22px;
+    height: 22px;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    opacity: 0.6;
+    z-index: 10;
+  }
+
+  a.router-link-exact-active .icon {
+    opacity: 1;
+  }
+
+  .icon.overview { background-image: var(--icon-overview-tab); }
+  .icon.asset    { background-image: var(--icon-asset-tab);    }
+  .icon.broker   { background-image: var(--icon-broker-tab);   }
+  .icon.history  { background-image: var(--icon-history-tab);  }
+
+  span {
+    color: var(--secondary-text-color);
+    margin-left: 20px;
+    cursor: pointer;
+  }
+
+  a.router-link-exact-active span {
+    color: var(--primary-text-color);
+  }
+
 </style>
