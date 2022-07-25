5bb7b31c857886e36088e9dcc7da65512cf42886
s.claes <sebastian.claes@protonmail.com>
2022-07-25 13:44:42 +0200

    [REFACTOR] changed :is component with the router view

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
-  <SidebarSection :activeTab="activeTabComponent"/>
-  <component
-      :is="activeTabComponent"
-      @nextComponent="changeTabComponent"
-  ></component>
+  <SidebarSection />
+  <router-view />
 </template>
 
 <script>
 import SidebarSection from '@/components/sidebar/SidebarSection'
-import OverviewTab    from '@/components/tab/overview/OverviewTab'
-import AssetTab       from '@/components/tab/asset/AssetTab'
-import BrokerTab      from '@/components/tab/broker/BrokerTab'
-import HistoryTab     from '@/components/tab/history/HistoryTab'
 
 export default {
   name: 'App',
   components: {
     SidebarSection,
-    OverviewTab,
-    AssetTab,
-    BrokerTab,
-    HistoryTab
-  },
-  data() {
-    return {
-      activeTabComponent: 'AssetTab',
-    }
-  },
-  methods: {
-    changeTabComponent (tabComponent) {
-      this.activeTabComponent = tabComponent;
-    },
   }
 }
 </script>
