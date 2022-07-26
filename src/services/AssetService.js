20d0db2a3a78f42fcd3c173d82bb3652e3d0900b
s.claes <sebastian.claes@protonmail.com>
2022-07-26 17:49:30 +0200

    [REFACTOR] removed fetch asset group from AssetService.js and renamed the fetchAsset function to fetchUnusedAsset, what is more accurate

--- src/services/AssetService.js
+++ src/services/AssetService.js
@@ -16,10 +16,7 @@ export default {
     // TODO: remove along with import after the api is finished (if it needs to be async have a look at commit cc178fc6cffe8eec8c9fbd3ea81c7c240a7ee8db)
     return assetGroups
   },
-  fetchAssetGroup(groupId) {
-    return apiClient.get('/asset-groups/' + groupId)
-  },
-  fetchAsset(assetId) {
+  fetchUnusedAsset(assetId) {
     return apiClient.get('/assets/' + assetId)
   }
 }
