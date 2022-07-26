82155d6249d90054fb77590441380a6cfbbdfd49
s.claes <sebastian.claes@protonmail.com>
2022-07-26 14:24:53 +0200

    [NEW] created the EditView.vue component

---
+++ src/views/EditView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{asset}}
+  </div>
+</template>
+
+<script>
+import AssetService from '@/services/AssetService.js'
+
+export default {
+  name: 'EditView',
+  props: {
+    assetId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      asset: null
+    }
+  },
+  created() {
+    AssetService.getAsset(this.id).then(response => {
+        this.asset = response.data
+    }).catch(error => {
+        console.log(error)
+    })
+  }
+}
+</script>
