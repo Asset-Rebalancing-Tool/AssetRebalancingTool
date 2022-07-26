975b979755ab8735fc7c4d250c9f8674fd633568
s.claes <sebastian.claes@protonmail.com>
2022-07-26 16:54:04 +0200

    [ADD] comment that refer to a commit that could be useful when solving the todo

--- src/services/AssetService.js
+++ src/services/AssetService.js
@@ -12,9 +12,8 @@ const apiClient = axios.create({
 
 export default {
   getAssetGroups() {
-    //return apiClient.get('/asset-groups')
-
-    // TODO: remove along with import after the api is finished
+    // return apiClient.get('/asset-groups')
+    // TODO: remove along with import after the api is finished (if it needs to be async have a look at commit cc178fc6cffe8eec8c9fbd3ea81c7c240a7ee8db)
     return assetGroups
   },
   getAssetGroup(groupId) {
