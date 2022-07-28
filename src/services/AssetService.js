b5f3b3cc01d6a39b72149847da5cbfb5e6585308
s.claes <sebastian.claes@protonmail.com>
2022-07-28 23:43:34 +0200

    [ADD] AssetService.js is now importing and fetching the groups and assets separately

--- src/services/AssetService.js
+++ src/services/AssetService.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import assetGroups from '@/data/assetGroups.json'
+import groupListObject from '@/data/groups.json'
+import assetListObject from '@/data/assets.json'
 
 const apiClient = axios.create({
   baseURL: 'https://my-json-server.typicode.com/',
@@ -11,10 +12,15 @@ const apiClient = axios.create({
 })
 
 export default {
-  fetchAssetGroups() {
+  fetchGroupList() {
     // return apiClient.get('/asset-groups')
     // TODO: remove along with import after the api is finished (if it needs to be async have a look at commit cc178fc6cffe8eec8c9fbd3ea81c7c240a7ee8db)
-    return assetGroups
+    return groupListObject
+  },
+  fetchAssetList() {
+    // return apiClient.get('/asset-groups')
+    // TODO: remove along with import after the api is finished (if it needs to be async have a look at commit cc178fc6cffe8eec8c9fbd3ea81c7c240a7ee8db)
+    return assetListObject
   },
   fetchUnusedAsset(assetId) {
     return apiClient.get('/assets/' + assetId)
