c6a8be1e8ca804816083c148660f94688966e31b
s.claes <sebastian.claes@protonmail.com>
2022-07-29 15:41:11 +0200

    [ADD] created move action wrapper class along with its styling and the prop to define whether the wrapper should be rendered or not

--- src/components/asset/searchbar/GroupContextMenu.vue
+++ src/components/asset/searchbar/GroupContextMenu.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-
+  <div class="move-action-wrapper" v-show="showWrapper">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'GroupContextMenu'
+  name: 'GroupContextMenu',
+  props: {
+    showWrapper: {
+      type: Boolean,
+      default: false
+    }
+  },
 }
 </script>
 
 <style scoped>
+  .move-action-wrapper {
+    position: absolute;
+    right: 0;
+    bottom: -410px;
+    width: 250px;
+    height: 400px;
+    background-color: var(--primary-background-color);
+    border: 1px solid var(--passive-border-color);
+    border-radius: var(--primary-border-radius);
+    box-shadow: 3px 3px 6px rgba(0,0,0,0.16);
+    z-index: 10;
+  }
 
+  .move-action-wrapper.active {
+    display: block;
+  }
 </style>
