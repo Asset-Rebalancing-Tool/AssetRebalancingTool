c2bc3c9698fdaf59b7cf2f700dfc28ffe98664e5
s.claes <sebastian.claes@protonmail.com>
2022-07-30 09:46:02 +0200

    [ADD] created template, styles and computed properties for the GroupContextMenu.vue component

--- src/components/asset/searchbar/GroupContextMenu.vue
+++ src/components/asset/searchbar/GroupContextMenu.vue
@@ -1,35 +1,150 @@
 <template>
   <div class="move-action-wrapper" v-show="showWrapper">
+
+    <header>
+      <p>Neue Gruppe erstellen</p>
+      <button>
+        <span class="icon group"></span>
+        <span class="icon plus"></span>
+      </button>
+    </header>
+
+    <label>
+      <input type="text" placeholder="Gruppen filtern">
+      <span class="icon search"></span>
+    </label>
+
+    <GroupAccordion
+        v-for="group in assetStore.groupListObject"
+        :key="group.id"
+        :thisGroup="group"
+    />
+
+    <GroupAccordion :thisGroup="{name: 'Keine Gruppe'}">
+        <li v-for="asset in assetStore.getAssetsWithoutGroup()"
+            :key="asset.id"
+            :class="{'selected': asset.isSelected}"
+        >{{asset.name}}</li>
+    </GroupAccordion>
   </div>
 </template>
 
 <script>
+import GroupAccordion from '@/components/asset/searchbar/GroupAccordion';
+import {useAssetStore} from '@/stores/AssetStore';
 export default {
   name: 'GroupContextMenu',
+  components: {
+    GroupAccordion
+  },
   props: {
     showWrapper: {
       type: Boolean,
       default: false
     }
   },
+  setup() {
+    const assetStore = useAssetStore()
+    return {
+      assetStore
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
   .move-action-wrapper {
     position: absolute;
     right: 0;
     bottom: -410px;
-    width: 250px;
-    height: 400px;
+    width: 280px;
     background-color: var(--primary-background-color);
     border: 1px solid var(--passive-border-color);
     border-radius: var(--primary-border-radius);
     box-shadow: 3px 3px 6px rgba(0,0,0,0.16);
     z-index: 10;
+    overflow: hidden;
   }
 
   .move-action-wrapper.active {
     display: block;
+  }
+
+  header {
+    display: flex;
+    padding: 15px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    color: var(--primary-text-color);
+    font-size: 18px;
+    line-height: 22px;
+    margin-top: 2px;
+  }
+
+  button {
+    width: 80px;
+    height: 48px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 8px;
+    border: 1px solid var(--passive-border-color);
+    background-color: var(--secondary-infill-color);
+    border-radius: var(--primary-border-radius);
+    cursor: pointer;
+  }
+
+  input {
+    width: calc(100% - 30px);
+    height: 46px;
+    padding: 0 18px;
+    margin: 0 15px 10px;
+  }
+
+  .icon {
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .icon.group {
+    width: 18px;
+    height: 18px;
+    background-image: var(--icon-group-action);
+  }
+  .icon.plus {
+    background-image: var(--icon-add);
+    width: 13px;
+    height: 13px;
+  }
+  .icon.search {
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    top: 50%;
+    right: 35px;
+    transform: translateY(-50%);
+    background-image: var(--icon-searchbar);
+  }
+
+  ul {
+    padding-left: 24px;
+    margin: 5px 0 10px;
+  }
+
+  li {
+    height: 26px;
+    line-height: 26px;
+    color: var(--primary-text-color);
+  }
+
+  li::marker {
+    font-size: 22px;
+  }
+
+  li.selected {
+    color: var(--main-color);
   }
 </style>
