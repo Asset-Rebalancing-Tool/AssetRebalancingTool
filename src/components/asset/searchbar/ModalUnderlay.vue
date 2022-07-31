e9e9638c1a587e2605fdbc1ed2125922ca160a6f
s.claes <sebastian.claes@protonmail.com>
2022-07-31 11:37:15 +0200

    [NEW] created ModalUnderlay.vue component, that will darken the underlay from a modal wrapper

---
+++ src/components/asset/searchbar/ModalUnderlay.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal-underlay" :class="{'active': assetStore.activeModalUnderlay}"></div>
+</template>
+
+<script>
+import {useAssetStore} from '@/stores/AssetStore';
+
+export default {
+  name: 'ModalUnderlay',
+  setup() {
+    const assetStore = useAssetStore()
+    return {
+      assetStore
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal-underlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000000;
+  opacity: 0;
+  z-index: 10;
+  pointer-events: none;
+  transition: 200ms ease-out;
+}
+
+.modal-underlay.active {
+  opacity: 0.45;
+  pointer-events: all;
+}
+
+</style>
