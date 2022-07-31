818d8ecf119f24a5aab6806b97f7acf5cf47cc4a
s.claes <sebastian.claes@protonmail.com>
2022-07-31 16:07:20 +0200

    [NEW] All from the vue cli add typescript command created files

---
+++ src/services/AssetService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import listOfGroups from '@/data/groups.json'
+import listOfAssets from '@/data/assets.json'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  fetchGroupList() {
+    // return apiClient.get('/asset-groups')
+    // TODO: remove along with import after the api is finished (if it needs to be async have a look at commit cc178fc6cffe8eec8c9fbd3ea81c7c240a7ee8db)
+    return listOfGroups
+  },
+  fetchAssetList() {
+    // return apiClient.get('/asset-groups')
+    // TODO: remove along with import after the api is finished (if it needs to be async have a look at commit cc178fc6cffe8eec8c9fbd3ea81c7c240a7ee8db)
+    return listOfAssets
+  },
+  fetchUnusedAsset(assetId) {
+    return apiClient.get('/assets/' + assetId)
+  }
+}
