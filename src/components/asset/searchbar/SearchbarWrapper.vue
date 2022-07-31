64fbaceff251a14cf8c47af73fc5074ee9267463
s.claes <sebastian.claes@protonmail.com>
2022-07-31 15:29:19 +0200

    [NEW] created SearchbarWrapper.vue component

---
+++ src/components/asset/searchbar/SearchbarWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="searchbar-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'SearchbarWrapper'
+}
+</script>
+
+<style scoped>
+  .searchbar-wrapper {
+    display: none;
+    position: absolute;
+    bottom: -40px; /* Input height 52px + Row gap 10px + Modal height Xpx */
+    left: 0;
+    width: 100%;
+    height: 50px;
+    padding: 0 25px;
+    background-color: var(--secondary-background-color);
+    border-radius: var(--primary-border-radius);
+  }
+</style>
