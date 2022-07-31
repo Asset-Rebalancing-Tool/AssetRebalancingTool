75cd7069d393161ee91078e4dc8f736e4d7418d5
s.claes <sebastian.claes@protonmail.com>
2022-07-31 11:47:25 +0200

    [ADD] removed empty white space and indent the css styles

--- src/components/asset/searchbar/ModalUnderlay.vue
+++ src/components/asset/searchbar/ModalUnderlay.vue
@@ -17,22 +17,21 @@ export default {
 </script>
 
 <style scoped>
-.modal-underlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: #000000;
-  opacity: 0;
-  z-index: 10;
-  pointer-events: none;
-  transition: 200ms ease-out;
-}
+  .modal-underlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: #000000;
+    opacity: 0;
+    z-index: 10;
+    pointer-events: none;
+    transition: 200ms ease-out;
+  }
 
-.modal-underlay.active {
-  opacity: 0.45;
-  pointer-events: all;
-}
-
+  .modal-underlay.active {
+    opacity: 0.45;
+    pointer-events: all;
+  }
 </style>
