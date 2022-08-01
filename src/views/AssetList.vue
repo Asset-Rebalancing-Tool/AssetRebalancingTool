fc770659262b50c128931dab02cf4a415930c8f4
s.claes <sebastian.claes@protonmail.com>
2022-08-01 14:34:32 +0200

    [REFACTOR] changed asset stores object names and changed assets id property into uuid

--- src/views/AssetList.vue
+++ src/views/AssetList.vue
@@ -9,16 +9,16 @@
     <TableFilters />
 
     <AssetGroup
-        v-for="group in assetStore.listOfGroups"
-        :key="group.id"
+        v-for="group in assetStore.ownedGroups"
+        :key="group.uuid"
         :thisGroup="group"
     />
 
     <AssetContainer
         v-for="asset in singleAssets"
-        :key="asset.id"
+        :key="asset.uuid"
         :thisAsset="asset"
-        @click="assetStore.toggleIsSelectedFlag(asset.id, asset.relatedGroupId)"
+        @click="assetStore.toggleIsSelectedFlag(asset.uuid, asset.relatedGroupUuid)"
     />
 
     <AssetListFooter />
@@ -30,14 +30,15 @@
 import { defineComponent } from 'vue';
 import { useAssetStore }   from '@/stores/AssetStore'
 
-import AssetSearchbar  from '../components/asset/searchbar/AssetSearchbar.vue'
-import ActionButtons   from '../components/asset/searchbar/ActionButtons.vue'
-import TableFilters    from '../components/asset/searchbar/TableFilters.vue'
-import AssetGroup      from '../components/asset/row/AssetGroup.vue'
-import AssetContainer  from '../components/asset/row/AssetContainer.vue'
-import AssetListFooter from '../components/asset/row/AssetListFooter.vue'
+import AssetSearchbar   from '../components/asset/searchbar/AssetSearchbar.vue'
+import ActionButtons    from '../components/asset/searchbar/ActionButtons.vue'
+import TableFilters     from '../components/asset/searchbar/TableFilters.vue'
+import AssetGroup       from '../components/asset/row/AssetGroup.vue'
+import AssetContainer   from '../components/asset/row/AssetContainer.vue'
+import AssetListFooter  from '../components/asset/row/AssetListFooter.vue'
+import { IOwnedAssets } from "@/models/IOwnedAssets";
 
-export defineComponent({
+export default defineComponent({
   name: 'AssetList',
   components: {
     AssetSearchbar,
@@ -52,7 +53,7 @@ export defineComponent({
     return { assetStore }
   },
   computed: {
-    singleAssets() {
+    singleAssets(): IOwnedAssets {
       return this.assetStore.getAssetsWithoutGroup()
     }
   }
