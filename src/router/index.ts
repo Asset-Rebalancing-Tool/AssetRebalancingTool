73db5126f6dcb2a97c40dd028370289e36454f30
s.claes <sebastian.claes@protonmail.com>
2022-08-02 10:43:26 +0200

    [ADD] new vite vue router index.ts configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,50 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-import PortfolioOverview   from '../views/PortfolioOverview.vue'
-import AssetList           from '../views/AssetList.vue'
-import EditAsset           from '../views/EditAsset.vue'
-import EditAssetGroup      from '../views/EditAssetGroup.vue'
-import ConnectBroker       from '../views/ConnectBroker.vue'
-import TransactionHistory  from '../views/TransactionHistory.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'PortfolioOverview',
-    component: PortfolioOverview
-  },
-  {
-    path: '/asset-list',
-    name: 'AssetList',
-    component: AssetList
-  },
-  {
-    path: '/edit-asset/:assetId',
-    name: 'EditAsset',
-    props: true,
-    component: EditAsset
-  },
-  {
-    path: '/edit-asset-group/:groupId',
-    name: 'EditAssetGroup',
-    props: true,
-    component: EditAssetGroup
-  },
-  {
-    path: '/connect-broker',
-    name: 'ConnectBroker',
-    component: ConnectBroker
-  },
-  {
-    path: '/transaction-history',
-    name: 'TransactionHistory',
-    component: TransactionHistory
-  }
-]
+import PortfolioOverview from '../views/PortfolioOverview.vue'
+import AssetList from '../views/AssetList.vue'
+import EditAsset from '../views/EditAsset.vue'
+import EditAssetGroup from '../views/EditAssetGroup.vue'
+import ConnectBroker from '../views/ConnectBroker.vue'
+import TransactionHistory from '../views/TransactionHistory.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'PortfolioOverview',
+      component: PortfolioOverview
+    },
+    {
+      path: '/asset-list',
+      name: 'AssetList',
+      component: AssetList
+    },
+    {
+      path: '/edit-asset/:assetId',
+      name: 'EditAsset',
+      props: true,
+      component: EditAsset
+    },
+    {
+      path: '/edit-asset-group/:groupId',
+      name: 'EditAssetGroup',
+      props: true,
+      component: EditAssetGroup
+    },
+    {
+      path: '/connect-broker',
+      name: 'ConnectBroker',
+      component: ConnectBroker
+    },
+    {
+      path: '/transaction-history',
+      name: 'TransactionHistory',
+      component: TransactionHistory
+    }
+  ]
 })
 
 export default router
