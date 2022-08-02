4ecb51dd54ef16793a9a7b981a07b53f49f9a0f6
s.claes <sebastian.claes@protonmail.com>
2022-08-02 18:59:44 +0200

    [ADD] removed unused unnecessary nav tag and refactored the styles by moving and restructuring them in a separated scss partial

--- src/components/sidebar/SidebarSection.vue
+++ src/components/sidebar/SidebarSection.vue
@@ -3,24 +3,22 @@
     <header>
       <div class="app-logo" />
     </header>
-    <nav>
-      <RouterLink class="overview" :to="{ name: 'PortfolioOverview' }">
-        <span class="icon overview"></span>
-        <span>Übersicht</span>
-      </RouterLink>
-      <RouterLink class="asset" :to="{ name: 'AssetList' }">
-        <span class="icon asset"></span>
-        <span>Assets verwalten</span>
-      </RouterLink>
-      <RouterLink class="broker" :to="{ name: 'ConnectBroker' }">
-        <span class="icon broker"></span>
-        <span>Broker Anbindung</span>
-      </RouterLink>
-      <RouterLink class="history" :to="{ name: 'TransactionHistory' }">
-        <span class="icon history"></span>
-        <span>Transaktionshistorie</span>
-      </RouterLink>
-    </nav>
+    <RouterLink class="overview" :to="{ name: 'PortfolioOverview' }">
+      <span class="icon overview"></span>
+      <span>Übersicht</span>
+    </RouterLink>
+    <RouterLink class="asset" :to="{ name: 'AssetList' }">
+      <span class="icon asset"></span>
+      <span>Assets verwalten</span>
+    </RouterLink>
+    <RouterLink class="broker" :to="{ name: 'ConnectBroker' }">
+      <span class="icon broker"></span>
+      <span>Broker Anbindung</span>
+    </RouterLink>
+    <RouterLink class="history" :to="{ name: 'TransactionHistory' }">
+      <span class="icon history"></span>
+      <span>Transaktionshistorie</span>
+    </RouterLink>
   </section>
 </template>
 
@@ -32,103 +30,6 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss" scoped>
-#sidebar {
-  min-width: 210px;
-  height: 100vh;
-  background-color: $secondary-background-color;
-  border-right: 1px solid $passive-border-color;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-header {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 30px 0;
-}
-
-.app-logo {
-  width: 100px;
-  height: 100px;
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-image: $app-logo;
-}
-
-nav {
-  width: 100%;
-}
-
-a {
-  position: relative;
-  width: 100%;
-  height: 48px;
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-  border: none;
-  text-decoration: none;
-  border-right: 5px solid $secondary-background-color;
-  background-color: transparent;
-}
-
-a.router-link-exact-active {
-  border-color: $main-color;
-}
-
-a.router-link-exact-active:before {
-  position: absolute;
-  top: 0;
-  right: 0;
-  height: 100%;
-  width: 80%;
-  content: '';
-  background-image: linear-gradient(
-    90deg,
-    rgba(25, 179, 153, 0),
-    rgba(25, 179, 153, 0.24)
-  );
-}
-
-.icon {
-  width: 22px;
-  height: 22px;
-  background-size: contain;
-  background-position: center;
-  background-repeat: no-repeat;
-  opacity: 0.6;
-  z-index: 10;
-}
-
-a.router-link-exact-active .icon {
-  opacity: 1;
-}
-
-.icon.overview {
-  background-image: $icon-portfolio-overview;
-}
-.icon.asset {
-  background-image: $icon-asset-list;
-}
-.icon.broker {
-  background-image: $icon-connect-broker;
-}
-.icon.history {
-  background-image: $icon-transaction-history;
-}
-
-span {
-  color: $secondary-text-color;
-  margin-left: 20px;
-  cursor: pointer;
-}
-
-a.router-link-exact-active span {
-  color: $primary-text-color;
-}
+<style lang="scss">
+@import '@/assets/scss/components/_sidebar.scss';
 </style>
