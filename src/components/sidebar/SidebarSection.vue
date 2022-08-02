1e408fa8f0dfcc16068a5139b07066b310e26d0e
s.claes <sebastian.claes@protonmail.com>
2022-08-02 20:26:54 +0200

    [REFACTOR] sidebar scss is now at its best

--- src/components/sidebar/SidebarSection.vue
+++ src/components/sidebar/SidebarSection.vue
@@ -3,19 +3,19 @@
     <header>
       <div class="app-logo" />
     </header>
-    <RouterLink class="overview" :to="{ name: 'PortfolioOverview' }">
+    <RouterLink :to="{ name: 'PortfolioOverview' }">
       <span class="icon overview"></span>
       <span>Übersicht</span>
     </RouterLink>
-    <RouterLink class="asset" :to="{ name: 'AssetList' }">
+    <RouterLink :to="{ name: 'AssetList' }">
       <span class="icon asset"></span>
       <span>Assets verwalten</span>
     </RouterLink>
-    <RouterLink class="broker" :to="{ name: 'ConnectBroker' }">
+    <RouterLink :to="{ name: 'ConnectBroker' }">
       <span class="icon broker"></span>
       <span>Broker Anbindung</span>
     </RouterLink>
-    <RouterLink class="history" :to="{ name: 'TransactionHistory' }">
+    <RouterLink :to="{ name: 'TransactionHistory' }">
       <span class="icon history"></span>
       <span>Transaktionshistorie</span>
     </RouterLink>
