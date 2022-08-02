fbd4cb9457d6bb0a2eeb7f7904b8c5bb9df7708c
s.claes <sebastian.claes@protonmail.com>
2022-08-02 17:35:45 +0200

    [FIX] import of scss variables

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,23 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
+  /**
+   * NOTE: All scss files that contains variables, need to be imported globally and separately!
+   * Globally because otherwise vite can't resolve the variables
+   * Separately because most of the theme scss files that actually contain variables, rely on another
+   */
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/styles.scss";`,
+        additionalData: `
+          @import "@/assets/scss/theme/_appearance.scss";
+          @import "@/assets/scss/theme/_color.scss";
+          @import "@/assets/scss/theme/_dimension.scss";
+          @import "@/assets/scss/theme/_font.scss";
+          @import "@/assets/scss/theme/_icon.scss";
+          @import "@/assets/scss/styles.scss";
+        `,
       },
     },
   },
