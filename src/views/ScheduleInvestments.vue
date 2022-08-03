42529a52eece409ac6fe2e7890521e76ed06ee60
s.claes <sebastian.claes@protonmail.com>
2022-08-03 09:12:29 +0200

    [NEW] created ScheduleInvestments.vue router view component

---
+++ src/views/ScheduleInvestments.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="content">
+    <label>Schedule Investments</label>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ScheduleInvestments',
+})
+</script>
