614ea7f8eee5c656a3b8d07a8ec1d5aa10ac6f8c
s.claes <sebastian.claes@protonmail.com>
2022-08-03 09:13:13 +0200

    [ADD] new router view components routing in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,8 @@ import EditAsset from '../views/EditAsset.vue'
 import EditAssetGroup from '../views/EditAssetGroup.vue'
 import ConnectBroker from '../views/ConnectBroker.vue'
 import TransactionHistory from '../views/TransactionHistory.vue'
+import ScheduleInvestments from '../views/ScheduleInvestments.vue'
+import AppSettings from '../views/AppSettings.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -41,6 +43,16 @@ const router = createRouter({
       name: 'TransactionHistory',
       component: TransactionHistory,
     },
+    {
+      path: '/investitionen-planen',
+      name: 'ScheduleInvestments',
+      component: ScheduleInvestments,
+    },
+    {
+      path: '/app-einstellungen',
+      name: 'AppSettings',
+      component: AppSettings,
+    }
   ],
 })
 
