5ab9dc1aa9a8581d5ecffb67939161681c6bb587
s.claes <sebastian.claes@protonmail.com>
2022-08-03 09:12:01 +0200

    [NEW] created AppSettings.vue router view component

---
+++ src/views/AppSettings.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="content">
+    <label>App Settings</label>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppSettings',
+})
+</script>
