5acbc3741117b1c79ee182f12834d4c14d1a150d
s.claes <sebastian.claes@protonmail.com>
2022-08-08 16:41:11 +0200

    [ADD] indent the script tag content

--- src/components/asset/group/AssetGroup.vue
+++ src/components/asset/group/AssetGroup.vue
@@ -24,40 +24,40 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-import { useAssetStore } from '@/stores/AssetStore'
-import type { PropType } from 'vue'
-import type { IOwnedPrivateGroup } from '@/models/old/IOwnedPrivateGroup'
-import AssetRow from '../row/AssetRow.vue'
-import TargetPercentageGrouping from '../row/column/TargetPercentageGrouping.vue'
-import AssetGroupFooter from './AssetGroupFooter.vue'
+  import { computed } from 'vue'
+  import { useAssetStore } from '@/stores/AssetStore'
+  import type { PropType } from 'vue'
+  import type { IOwnedPrivateGroup } from '@/models/old/IOwnedPrivateGroup'
+  import AssetRow from '../row/AssetRow.vue'
+  import TargetPercentageGrouping from '../row/column/TargetPercentageGrouping.vue'
+  import AssetGroupFooter from './AssetGroupFooter.vue'
 
-const assetStore = useAssetStore()
+  const assetStore = useAssetStore()
 
-const props = defineProps({
-  thisGroup: {
-    type: Object as PropType<IOwnedPrivateGroup>,
-    required: true,
-  }
-})
+  const props = defineProps({
+    thisGroup: {
+      type: Object as PropType<IOwnedPrivateGroup>,
+      required: true,
+    }
+  })
 
-// Returns a bool that indicates if the assets object is empty or not
-const isEmptyGroup = computed(() => {
-  return props.thisGroup.relatedAssetsUuidArray.length === 0
-})
+  // Returns a bool that indicates if the assets object is empty or not
+  const isEmptyGroup = computed(() => {
+    return props.thisGroup.relatedAssetsUuidArray.length === 0
+  })
 
-// Render the selected class based in the groups isSelected flag
-const isSelectedGroup = computed(() => {
-  return props.thisGroup.isSelected ? 'selected' : ''
-})
+  // Render the selected class based in the groups isSelected flag
+  const isSelectedGroup = computed(() => {
+    return props.thisGroup.isSelected ? 'selected' : ''
+  })
 
-// Render the empty-group class based on the isEmptyGroup bool
-const emptyGroupClass = computed(() => {
-  return isEmptyGroup ? 'empty-group' : ''
-})
+  // Render the empty-group class based on the isEmptyGroup bool
+  const emptyGroupClass = computed(() => {
+    return isEmptyGroup ? 'empty-group' : ''
+  })
 
-// Get an object of all assets, that are nested in that group
-const relatedAssets = computed(() => {
-  return assetStore.getAssetsByGroupUuid(props.thisGroup.uuid)
-})
+  // Get an object of all assets, that are nested in that group
+  const relatedAssets = computed(() => {
+    return assetStore.getAssetsByGroupUuid(props.thisGroup.uuid)
+  })
 </script>
