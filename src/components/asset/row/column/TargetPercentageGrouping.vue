aa12ddb5be44ef7565bd3d931ced4c19139e3a1e
s.claes <sebastian.claes@protonmail.com>
2022-08-08 16:10:14 +0200

    [REFACTOR] TargetPercentageGrouping.vue component is now using the composition api instead of the options api

--- src/components/asset/row/column/TargetPercentageGrouping.vue
+++ src/components/asset/row/column/TargetPercentageGrouping.vue
@@ -10,43 +10,34 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import type { PropType } from 'vue'
-import type { IOwnedPublicAssets } from '@/models/old/IOwnedPublicAssets'
-import { useAssetStore } from '@/stores/AssetStore'
+<script lang="ts" setup>
+  import { useAssetStore } from '@/stores/AssetStore'
+  import {computed} from "vue";
 
-export default defineComponent({
-  name: 'TargetPercentageGrouping',
-  props: {
+  const assetStore = useAssetStore()
+
+  const props = defineProps({
     assets: {
-      type: Object as PropType<IOwnedPublicAssets>,
+      type: Array,
       required: true,
     },
-  },
-  computed: {
-    // Calculate the height of the target percentage grouping
-    targetPercentageGroupingHeight(): string {
-      // Check for how many asset rows the wrapper height has to be adjusted
-      const assetCount = Object.keys(this.assets).length - 1
+  })
 
-      // Css variables in _dimensions.scss
-      // TODO: get variables from dom elements
-      const header = 16
-      const border = 3
-      const innerPadding = 3
-      const secondaryInputHeight = 32
-      const innerFrame = innerPadding * 2 + secondaryInputHeight
-      const rowGap = 57
+  // Calculate the height of the target percentage grouping
+  const targetPercentageGroupingHeight = computed(() => {
 
-      return border * 2 + header + innerFrame + rowGap * assetCount + 'px'
-    },
-  },
-  setup() {
-    const assetStore = useAssetStore()
-    return {
-      assetStore,
-    }
-  },
-})
+    // Check for how many asset rows the wrapper height has to be adjusted
+    const assetCount = Object.keys(this.assets).length - 1
+
+    // Css variables in _dimensions.scss
+    // TODO: get variables from dom elements
+    const header = 16
+    const border = 3
+    const innerPadding = 3
+    const secondaryInputHeight = 32
+    const innerFrame = innerPadding * 2 + secondaryInputHeight
+    const rowGap = 57
+
+    return border * 2 + header + innerFrame + rowGap * assetCount + 'px'
+  })
 </script>
