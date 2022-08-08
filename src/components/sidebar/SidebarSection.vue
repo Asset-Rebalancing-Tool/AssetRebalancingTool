92900a4f2a8f2bc635a10e320a6136cdb9e706b5
s.claes <sebastian.claes@protonmail.com>
2022-08-08 15:06:34 +0200

    [REFACTOR] SidebarSection.vue component is now using the composition api instead of the options api

--- src/components/sidebar/SidebarSection.vue
+++ src/components/sidebar/SidebarSection.vue
@@ -3,46 +3,29 @@
     <header>
       <div class="app-logo" />
     </header>
-    <RouterLink :to="{ name: 'PortfolioOverview' }">
-      <span class="icon overview"></span>
-      <span>Übersicht</span>
-    </RouterLink>
-    <RouterLink :to="{ name: 'ConnectBroker' }">
-      <span class="icon broker"></span>
-      <span>Broker Anbindung</span>
-    </RouterLink>
-    <RouterLink :to="{ name: 'AssetList' }">
-      <span class="icon asset"></span>
-      <span>Assets verwalten</span>
-    </RouterLink>
-    <RouterLink :to="{ name: 'TransactionHistory' }">
-      <span class="icon history"></span>
-      <span>Transaktionshistorie</span>
-    </RouterLink>
-    <RouterLink :to="{ name: 'ScheduleInvestments' }">
-      <span class="icon invest"></span>
-      <span>Investitionen planen</span>
-    </RouterLink>
-    <RouterLink :to="{ name: 'AppSettings' }">
-      <span class="icon settings"></span>
-      <span>Einstellungen</span>
-    </RouterLink>
 
-    <div v-if="false" class="logout-wrapper">
-      <span class="icon logout"></span>
-      <span>Abmelden</span>
-    </div>
+    <RouterLink
+        v-for="link in sidebarLinks"
+        :key="link.component"
+        :to="{ name: link.component }"
+    >
+      <span class="icon" :class="link.component"></span>
+      <span>{{ link.label }}</span>
+    </RouterLink>
   </section>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'SidebarSection',
-})
+<script lang="ts" setup>
+  const sidebarLinks = [
+    { component: 'PortfolioOverview',   label: 'Übersicht'            },
+    { component: 'ConnectBroker',       label: 'Broker Anbindung'     },
+    { component: 'AssetList',           label: 'Assets verwalten'     },
+    { component: 'TransactionHistory',  label: 'Transaktionshistorie' },
+    { component: 'ScheduleInvestments', label: 'Investitionen planen' },
+    { component: 'AppSettings',         label: 'Einstellungen'        }
+  ];
 </script>
 
 <style lang="scss">
-@import '@/assets/scss/components/sidebar/_sidebar.scss';
+  @import '@/assets/scss/components/sidebar/_sidebar.scss';
 </style>
