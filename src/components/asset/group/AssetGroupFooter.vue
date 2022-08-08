5a625cc7c464d74d83a1bd126149eec19f4b8320
s.claes <sebastian.claes@protonmail.com>
2022-08-08 16:36:02 +0200

    [REFACTOR] AssetGroupFooter.vue component is now using the composition api instead of the options api

--- src/components/asset/group/AssetGroupFooter.vue
+++ src/components/asset/group/AssetGroupFooter.vue
@@ -19,59 +19,43 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useAssetStore } from '@/stores/AssetStore'
-import type { PropType } from 'vue'
-import type { IOwnedPrivateGroup } from '@/models/old/IOwnedPrivateGroup'
-import ActualValueColumn from '../row/column/ActualValueColumn.vue'
-import ColumnInput from '../row/column/ColumnInput.vue'
-import SingleValue from '../row/column/SingleValue.vue'
+<script lang="ts" setup>
+  import { computed } from "vue";
+  import { useAssetStore } from '@/stores/AssetStore'
+  import type { PropType } from 'vue'
+  import type { IOwnedPrivateGroup } from '@/models/old/IOwnedPrivateGroup'
+  import ActualValueColumn from '../row/column/ActualValueColumn.vue'
+  import ColumnInput from '../row/column/ColumnInput.vue'
+  import SingleValue from '../row/column/SingleValue.vue'
 
-export default defineComponent({
-  name: 'AssetGroupFooter',
-  components: {
-    SingleValue,
-    ColumnInput,
-    ActualValueColumn,
-  },
-  props: {
+  const assetStore = useAssetStore()
+
+  const props = defineProps({
     thisGroup: {
       type: Object as PropType<IOwnedPrivateGroup>,
       required: true,
-    },
-  },
-  computed: {
-    // The total value of this group
-    totalValue(): number {
-      return this.thisGroup.totalValue
-    },
-
-    // The total percentage of this group
-    totalPercentage(): number {
-      return this.thisGroup.totalPercentage
-    },
-
-    // The total target percentage of this group
-    totalTargetPercentage(): string[] {
-      return this.assetStore.getValueArray(this.thisGroup.totalTargetPercentage)
-    },
-
-    // The total deviation of this group
-    totalDeviation(): string[] {
-      return this.assetStore.getValueArray(this.thisGroup.totalDeviation)
-    },
-  },
-  setup() {
-    const assetStore = useAssetStore()
-    return {
-      assetStore,
     }
-  },
-})
+  })
+
+
+  // The total value of this group
+  const totalValue = computed(() => props.thisGroup.totalValue)
+
+  // The total percentage of this group
+  const totalPercentage = computed(() => props.thisGroup.totalPercentage)
+
+  // The total target percentage of this group
+  const totalTargetPercentage = computed(() => {
+    return assetStore.getValueArray(props.thisGroup.totalTargetPercentage)
+  })
+
+  // The total deviation of this group
+  const totalDeviation = computed(() => {
+    return assetStore.getValueArray(props.thisGroup.totalDeviation)
+  })
 </script>
 
 <!-- not scoped !-->
 <style lang="scss">
-@import '../../../assets/scss/components/asset/group/asset-group-footer';
+  @import '../../../assets/scss/components/asset/group/asset-group-footer';
 </style>
