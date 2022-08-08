42c3c473157dcba7107e36f0ce7f3458dc30a919
s.claes <sebastian.claes@protonmail.com>
2022-08-08 15:46:47 +0200

    [REFACTOR] SearchbarContentWrapper.vue component is now using the composition api instead of the options api

--- src/components/asset/searchbar/SearchbarContentWrapper.vue
+++ src/components/asset/searchbar/SearchbarContentWrapper.vue
@@ -4,65 +4,33 @@
     :class="{ active: assetStore.activeModalUnderlay }"
   >
     <div class="searchbar-label-grid">
-      <p>Ergebnisse ({{ fetchedAssets.length }})</p>
+      <p>Ergebnisse ({{ props.fetchedAssets.length }})</p>
     </div>
-
     <SearchbarAsset
       v-for="asset in fetchedAssets"
       :key="asset.uuid"
       :thisAsset="asset"
     />
-
     <div class="divider"></div>
-
-    <div class="searchbar-content-footer">
-      <div class="footer-info">
-        <p>Ist dein Asset nicht dabei?</p>
-        <p>
-          Erstelle eigene Assets oder Gruppen, um dein Portfolio vollumfänglich
-          abzubilden.
-        </p>
-      </div>
-      <div class="button-wrapper">
-        <button><span class="icon"></span>Asset erstellen</button>
-        <button><span class="icon"></span>Gruppe anlegen</button>
-      </div>
-    </div>
+    <SearchbarFooter />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
 import { useAssetStore } from '@/stores/AssetStore'
 import SearchbarAsset from '@/components/asset/searchbar/SearchbarAsset.vue'
-import type { IOwnedPublicAssets } from '@/models/old/IOwnedPublicAssets'
+import SearchbarFooter from "@/components/asset/searchbar/SearchbarFooter.vue";
 
-export default defineComponent({
-  name: 'SearchbarContentWrapper',
-  components: {
-    SearchbarAsset,
-  },
-  props: {
-    fetchedAssets: {
-      type: Array,
-      required: true
-    }
-  },
-  computed: {
-    singleAssets(): IOwnedPublicAssets {
-      return this.assetStore.getAssetsWithoutGroup()
-    },
-  },
-  setup() {
-    const assetStore = useAssetStore()
-    return {
-      assetStore,
-    }
-  },
+const assetStore = useAssetStore()
+const props = defineProps({
+  fetchedAssets: {
+    type: Array,
+    required: true
+  }
 })
 </script>
 
 <!-- not scoped -->
 <style lang="scss">
-@import 'src/assets/scss/components/asset/searchbar/searchbar-content-wrapper.scss';
+  @import 'src/assets/scss/components/asset/searchbar/searchbar-content-wrapper.scss';
 </style>
