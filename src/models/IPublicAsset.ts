a239e183da8da8471fb54fc6fc8bbc708c7057ac
s.claes <sebastian.claes@protonmail.com>
2022-08-10 09:55:01 +0200

    [ADD] updated IPublicAssets.ts interface, in order

--- src/models/IPublicAsset.ts
+++ src/models/IPublicAsset.ts
@@ -1,15 +1,15 @@
-import type { IPriceRecords } from '@/models/IPriceRecords'
+import type { IPriceRecord } from '@/models/IPriceRecord'
 import type { IAssetInformation } from '@/models/IAssetInformation'
 import type { IUiProperties } from '@/models/IUiProperties'
 
 export interface IPublicAsset {
   uuid: string
-  assetInformations?: IAssetInformation | null
+  assetInformations?: IAssetInformation[] | null
   assetName: string
   assetType: string
   isin: string
-  priceRecords: IPriceRecords
+  priceRecords?: IPriceRecord[] | null
   symbol: string
   ownedQuantity: string
-  //uiProperties?: IUiProperties | null
+  uiProperties?: IUiProperties | null
 }
