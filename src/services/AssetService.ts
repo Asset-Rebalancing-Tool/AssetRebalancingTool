09763bf2136cdac5256d6aa30e7a3d141b09f71a
s.claes <sebastian.claes@protonmail.com>
2022-08-11 19:03:48 +0200

    [ADD] removed async searchAssets function from the asses service since it is located in the SearchbarAsset.vue component

--- src/services/AssetService.ts
+++ src/services/AssetService.ts
@@ -1,10 +1,7 @@
-import axios from 'axios'
-import type { AxiosResponse } from 'axios'
 import ownedGroups from '../data/ownedGroups.json'
 import ownedAssets from '../data/ownedAssets.json'
 import type { IOwnedPrivateGroups } from '@/models/old/IOwnedPrivateGroups'
 import type { IOwnedPublicAssets } from '@/models/old/IOwnedPublicAssets'
-import type { IPublicAsset } from "@/models/IPublicAsset";
 
 export default {
   fetchOwnedGroups(): IOwnedPrivateGroups {
@@ -12,25 +9,5 @@ export default {
   },
   fetchOwnedAssets(): IOwnedPublicAssets {
     return ownedAssets as IOwnedPublicAssets
-  },
-
-  async searchAssets(searchString: string): Promise<IPublicAsset[]> {
-    return axios
-      .post(
-        '/api/asset_api/asset/search',
-        {
-          SearchString: searchString,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-          },
-        }
-      )
-      .then((response: AxiosResponse<IPublicAsset[]>) => {
-        return response.data
-      })
-
-  },
+  }
 }
