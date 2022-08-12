6b1610469be2bd5e570cc9002a106146f9ebce4c
s.claes <sebastian.claes@protonmail.com>
2022-08-12 13:15:46 +0200

    [ADD] created checkWordMatches compute property, that creates a string with a html marker tag where the user input matches the asset name

--- src/components/asset/row/column/InfoColumn.vue
+++ src/components/asset/row/column/InfoColumn.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="info-wrapper">
-    <p>{{ props.name }}</p>
+    <p v-html="checkWordMatches"></p>
     <div class="info-row">
-      <div class="type">{{ props.type }}</div>
+      <div class="type">{{ type }}</div>
       <span class="isin label">ISIN</span>
       <span class="isin-wrapper" @click="copyISIN($event)">
-        <span class="isin">{{ props.isin }}</span>
+        <span class="isin">{{ isin }}</span>
         <span class="icon"></span>
       </span>
     </div>
@@ -13,9 +13,13 @@
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue";
+import { useAssetStore } from '@/stores/AssetStore'
+
+const assetStore = useAssetStore()
 
 const props = defineProps({
-  name: {
+  assetName: {
     type: String,
     required: true,
   },
@@ -26,7 +30,7 @@ const props = defineProps({
   isin: {
     type: String,
     required: false,
-  },
+  }
 })
 
 // Copy the isin of an asset into the clipboard
@@ -42,4 +46,18 @@ const copyISIN = (event: Event) => {
     alert('Copied the text: ' + tempTextInput.value)
   }
 }
+
+const checkWordMatches = computed((): string => {
+  let input: string = assetStore.searchString.toLowerCase()
+  let assetName: string = props.assetName.toLowerCase()
+
+  if (assetName.includes(input)) {
+    let indexStart: number = assetName.indexOf(input);
+    let indexEnd: number = indexStart + input.length
+    return props.assetName.substring(0, indexStart)
+        + '<mark>' + props.assetName.substring(indexStart, indexEnd) +'</mark>'
+        + props.assetName.substring(indexEnd)
+  }
+  return props.assetName
+})
 </script>
