9e2dcc3c2c3bfae10b7d6a313165d1504ca58c7a
s.claes <sebastian.claes@protonmail.com>
2022-08-12 11:28:45 +0200

    [ADD] created SearchbarSkeleton.vue component

---
+++ src/components/asset/searchbar/SearchbarSkeleton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="searchbar-asset-skeleton">
+    <div class="skeleton-logo"></div>
+
+    <div class="skeleton-info-wrapper">
+      <div class="name"></div>
+      <div class="info"></div>
+    </div>
+
+    <div class="skeleton-price"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/scss/components/asset/searchbar/_skeleton-loader.scss';
+</style>
