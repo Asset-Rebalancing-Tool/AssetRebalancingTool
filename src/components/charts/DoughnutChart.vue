0cd4c5346b5818e2f73639af03f161c70ddb34d8
s.claes <sebastian.claes@protonmail.com>
2022-08-13 11:39:21 +0200

    [NEW] created DoughnutChart.vue component

---
+++ src/components/charts/DoughnutChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <DoughnutChart
+      :chart-data="data"
+      :options="options"
+      css-classes="chart-container"
+  />
+</template>
+
+<script setup>
+  import { ref, computed } from 'vue'
+  import { DoughnutChart } from 'vue-chart-3'
+  import { Chart, DoughnutController, ArcElement } from 'chart.js'
+
+  Chart.register(DoughnutController, ArcElement)
+
+  const dataValues = ref([10, 20, 40])
+
+  const data = computed(() => ({
+    labels: ['Foo', 'Bar', 'Baz'],
+    datasets: [
+        {
+          label: 'Foo',
+          data: dataValues.value,
+          backgroundColor: ['#859900', '#d33682', '#cb4b16']
+        }
+    ]
+  }))
+
+  const options = ref({
+    plugins: {
+      title: {
+        text: 'Doughnut'
+      }
+    }
+  })
+
+</script>
