e8b72b888b823e0ea2861f1592ca790c4ab5c67c
s.claes <sebastian.claes@protonmail.com>
2022-08-13 11:36:51 +0200

    [ADD] created chart.js main config js file and included into the main.ts file

---
+++ src/plugins/chart.js
@@ -0,0 +1,9 @@
+import { Chart, Legend, Title, Tooltip } from "chart.js";
+
+Chart.register(Legend, Title, Tooltip)
+
+Chart.defaults.plugins.title.display = false
+Chart.defaults.plugins.legend.display = false
+
+// General setting can be made here
+// https://www.chartjs.org/docs/latest/general/
