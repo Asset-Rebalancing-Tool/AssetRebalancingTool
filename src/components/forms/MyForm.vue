ae7e705a25c97a12f0341c8b80ab37552cb5d79a
s.claes <sebastian.claes@protonmail.com>
2022-08-18 10:00:22 +0200

    [NEW] base form component to test the new input vue components

---
+++ src/components/forms/MyForm.vue
@@ -0,0 +1,25 @@
+<form>
+
+
+<BaseInput
+    v-model="event.title"
+    label="Title"
+    type="text"
+/>
+
+<button type="submit">Submit</button>
+</form>
+
+<script setup>
+import BaseInput from 'src/components/forms/fields/BaseInput.vue'
+import { reactive } from "vue/dist/vue";
+
+
+// The components reactive state object
+const state = reactive({
+  publicAssets: [],
+  resultCount: 0,
+  timer: null,
+  isLoading: false,
+})
+</script>
