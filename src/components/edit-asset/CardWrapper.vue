a0485d88267e0dd170ace5ff8e4840fe4cb1dd9a
s.claes <sebastian.claes@protonmail.com>
2022-08-18 12:01:45 +0200

    [NEW] created the EditAsset views CardWrapper.vue child component

---
+++ src/components/edit-asset/CardWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <p>{{ headerText }}</p>
+    </div>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  headerText: {
+    type: String,
+    required: true,
+  },
+})
+</script>
