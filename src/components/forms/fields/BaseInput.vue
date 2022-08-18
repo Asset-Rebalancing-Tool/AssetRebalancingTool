4cd94a6c64f23c0d0c857ed508c8d404f14954b6
s.claes <sebastian.claes@protonmail.com>
2022-08-18 09:59:52 +0200

    [NEW] BaseInput.vue form component

---
+++ src/components/forms/fields/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input
+      v-bind="$attrs"
+      :value="modelValue"
+      :placeholder="label"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="field"
+  >
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: 'String',
+    default: ''
+  },
+  modelValue: {
+    type: [String, Number],
+    default: ''
+  }
+})
+</script>
