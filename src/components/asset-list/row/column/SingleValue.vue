744395b422b287f1959125a72c8046d2c58fe125
s.claes <sebastian.claes@protonmail.com>
2022-08-18 13:21:15 +0200

    [REFACTOR] exchanged LineChart component with slot

--- src/components/asset-list/row/column/SingleValue.vue
+++ src/components/asset-list/row/column/SingleValue.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="single-value-wrapper" :class="smallerGrid">
-    <LineChart
-      v-if="showGraph"
-      :data-values="dataValues"
-      :data-labels="dataLabels"
-      :border-width="'0.8'"
-      :background-color="'#19B399'"
-      :border-color="'#19B399'"
-    />
+    <slot name="graph"></slot>
     <span class="first-digit">{{ firstDigit }}</span>
     <span class="decimal-wrapper">
       <span class="first-decimal">{{ firstDecimal }}</span>
@@ -20,7 +13,6 @@
 
 <script lang="ts" setup>
 import { computed } from 'vue'
-import LineChart from '@/components/charts/LineChart.vue'
 import type { IPriceRecord } from '@/models/nested/IPriceRecord'
 
 const props = defineProps({
@@ -52,34 +44,8 @@ const showArrow = computed(() => {
   return props.arrow === 'up' || props.arrow === 'down'
 })
 
-const showGraph = computed(() => {
-  return props.priceRecords.length > 0
-})
-
 // Render the additional grid class
 const smallerGrid = computed(() => {
   return showArrow.value ? 'smaller' : ''
-})
-
-const dataValues = computed((): number[] => {
-  const dataValues: number[] = []
-  if (props.priceRecords) {
-    for (const item of props.priceRecords) {
-      const record = item as IPriceRecord
-      dataValues.push(record.price)
-    }
-  }
-  return dataValues
-})
-
-const dataLabels = computed((): string[] => {
-  const dataLabels: string[] = []
-  if (props.priceRecords) {
-    for (const item of props.priceRecords) {
-      const record = item as IPriceRecord
-      dataLabels.push(record.tsPrice)
-    }
-  }
-  return dataLabels
 })
 </script>
