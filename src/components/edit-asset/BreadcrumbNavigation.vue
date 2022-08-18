58fc44b4d55b9593a24c4aaf901bd8666b262790
s.claes <sebastian.claes@protonmail.com>
2022-08-18 12:01:16 +0200

    [NEW] created the EditAsset views BreadcrumbNavigation.vue child component

---
+++ src/components/edit-asset/BreadcrumbNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="breadcrumb-wrapper">
+    <RouterLink :to="{ name: 'AssetList' }">Assets verwalten</RouterLink>
+    <div class="arrow"></div>
+    <a>hinzufügen</a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
