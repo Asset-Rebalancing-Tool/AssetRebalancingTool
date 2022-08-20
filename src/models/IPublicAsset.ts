a64bfff3a5a73a697eca183a6e4b1eb123746df0
tobi97h <tobi_h@outlook.com>
2022-08-20 14:15:27 +0200

    change proxy ip & create currency map type

--- src/models/IPublicAsset.ts
+++ src/models/IPublicAsset.ts
@@ -1,15 +1,18 @@
 import type { IPriceRecord } from '@/models/nested/IPriceRecord'
 import type { IAssetInformation } from '@/models/nested/IAssetInformation'
 
+type CurrencyPriceRecordMap = {
+  [key: string]: IPriceRecord[];
+};
+
 export interface IPublicAsset {
   uuid: string
-  currencyPriceRecordMap: object
+  currencyPriceRecordMap: CurrencyPriceRecordMap
   assetInformations: IAssetInformation[]
   assetName: string
   assetType: string
   symbol: string | null
   isin: string | null
-  priceRecords: IPriceRecord[]
   currency: string
   priceType: string
   icon: string | null
