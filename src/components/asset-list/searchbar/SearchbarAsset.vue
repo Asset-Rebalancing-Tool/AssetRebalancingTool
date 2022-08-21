4c5ca287a2571c534a1adcd71954be5735584e56
s.claes <sebastian.claes@protonmail.com>
2022-08-21 16:36:47 +0200

    [ADD] template slot for the LineChart in the SearchbarAsset.vue component

--- src/components/asset-list/searchbar/SearchbarAsset.vue
+++ src/components/asset-list/searchbar/SearchbarAsset.vue
@@ -1,8 +1,9 @@
 <template>
   <RouterLink
+    :disabled="quickCreate"
     :to="{ name: 'EditAsset', params: { uuid: thisAsset.uuid } }"
     class="searchbar-asset"
-    @click="hideModalUnderlay"
+    @click="newAssetAction"
   >
     <div class="searchbar-asset-logo"></div>
 
@@ -16,7 +17,18 @@
       :asset-uuid="thisAsset.uuid"
       :value-array="priceArray"
       :unit="currency"
-    />
+    >
+      <template #graph>
+        <LineChart
+            v-if="showGraph"
+            :data-values="getDataValues(thisAsset.uuid)"
+            :data-labels="getDataLabels(thisAsset.uuid)"
+            :border-width="'0.8'"
+            :background-color="'#19B399'"
+            :border-color="'#19B399'"
+        />
+      </template>
+    </SingleValue>
   </RouterLink>
 </template>
 
@@ -26,10 +38,17 @@ import type { PropType } from 'vue'
 import type { IPublicAsset } from '@/models/IPublicAsset'
 import { getNewestPriceRecordFormatted } from '@/composables/valueArray'
 import { hideModalUnderlay } from '@/composables/modalOverlay'
-import { getAssetCurrency } from '@/composables/currency';
+import { getAssetCurrency } from '@/composables/currency'
 import InfoColumn from '../row/column/InfoColumn.vue'
 import SingleValue from '../row/column/SingleValue.vue'
+import { useAssetStore } from '@/stores/AssetStore'
+import { showGraph, getDataValues, getDataLabels } from '@/composables/smallLineChart';
+import LineChart from '@/components/charts/LineChart.vue';
 
+const assetStore = useAssetStore()
+
+// Temporary hardcoded flag
+const quickCreate = false
 
 const props = defineProps({
   thisAsset: {
@@ -37,6 +56,15 @@ const props = defineProps({
     required: true,
   },
 })
+
+function newAssetAction(): void {
+  // Hide the modal underlay, no matter what creation will be fired
+  hideModalUnderlay()
+  // If the quick create flag is true, the router link will be disabled and this method will be fired
+  if (quickCreate) {
+    //assetStore.
+  }
+}
 
 // Get an array that contains the exploded strings of a price record
 const priceArray = computed((): string[] => {
