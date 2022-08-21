028da41d76474ef1fff9df70dc04b9c0ffff14d4
s.claes <sebastian.claes@protonmail.com>
2022-08-21 16:11:55 +0200

    [ADD] return empty object in AssetService.ts fetch own groups and fetch own assets, because the graph wouldn't be rendered properly and throw an error, since the data structure changed

--- src/services/AssetService.ts
+++ src/services/AssetService.ts
@@ -1,5 +1,3 @@
-import ownedGroups from '../data/ownedGroups.json'
-import ownedAssets from '../data/ownedAssets.json'
 import type { IOwnedPrivateGroups } from '@/models/old/IOwnedPrivateGroups'
 import type { IOwnedPublicAssets } from '@/models/old/IOwnedPublicAssets'
 import type { IPublicAsset } from '@/models/IPublicAsset';
@@ -8,10 +6,12 @@ import axios from "axios";
 
 export default {
   fetchOwnedGroups(): IOwnedPrivateGroups {
-    return ownedGroups as IOwnedPrivateGroups
+    // return ownedGroups as IOwnedPrivateGroups
+    return {} as IOwnedPrivateGroups
   },
   fetchOwnedAssets(): IOwnedPublicAssets {
-    return ownedAssets as IOwnedPublicAssets
+    //return ownedAssets as IOwnedPublicAssets
+    return {} as IOwnedPublicAssets
   },
   fetchPublicAssets(searchValue: string): Promise<IPublicAsset[]> {
     return axios
