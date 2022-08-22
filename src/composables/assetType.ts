57aadd9775a492057ea511747857e01094291c9f
s.claes <sebastian.claes@protonmail.com>
2022-08-22 14:14:45 +0200

    [NEW] created assetType.ts composable

---
+++ src/composables/assetType.ts
@@ -0,0 +1,39 @@
+import { useAssetStore } from "@/stores/AssetStore";
+import type { IPublicAsset } from '@/models/IPublicAsset';
+import type { AssetTypeEnum } from "@/models/enums/AssetTypeEnum";
+
+/**
+ * Get the asset type by passing the uuid of an asset
+ *
+ * @param uuid string
+ */
+export function mapAssetTypeByUUid(uuid: string) {
+    const assetStore = useAssetStore()
+    const asset: IPublicAsset = assetStore.getSearchbarAsset(uuid)
+    return mapAssetType(asset.assetType)
+}
+
+/**
+ * Map the asset type to its german value
+ *
+ * @param assetType AssetTypeEnum
+ */
+function mapAssetType(assetType: AssetTypeEnum): string {
+    switch (assetType) {
+        default:
+        case 'Other':
+            return 'Anderes'
+        case 'Stock':
+            return 'Aktie'
+        case 'Fond':
+            return 'Fond'
+        case 'Etf':
+            return 'ETF'
+        case 'Commodity':
+            return 'Rohstoff'
+        case 'PreciousMetals':
+            return 'Edelmetall'
+        case 'Crypto':
+            return 'Kryptowährung'
+    }
+}
