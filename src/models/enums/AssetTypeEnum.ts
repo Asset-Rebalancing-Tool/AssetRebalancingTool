df6b4fccc7eb7dae12d6442e7f871a9a79a0e1b7
s.claes <sebastian.claes@protonmail.com>
2022-08-22 14:12:11 +0200

    [NEW] created AssetTypeEnum.ts and adjusted CurrencyEnum.ts

---
+++ src/models/enums/AssetTypeEnum.ts
@@ -0,0 +1,9 @@
+export enum AssetTypeEnum {
+  OTHER = 'Other',
+  STOCK = 'Stock',
+  FOND = 'Fond',
+  ETF = 'Etf',
+  COMMODITY = 'Commodity',
+  PRECIOUS_METALS = 'PreciousMetals',
+  CRYPTO = 'Crypto'
+}
