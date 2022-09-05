0ece51a32add4be1e409bff69a7934a876742e4d
s.claes <sebastian.claes@protonmail.com>
2022-09-05 17:32:52 +0200

    [NEW] created check icon svg component for the asset list footer

---
+++ src/assets/icons/IconCheck.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="15.451" height="11.846" viewBox="0 0 15.451 11.846">
+    <path id="check" d="M0,21.1l1.717-2.232L6.18,22.649l7.211-8.069,2.06,1.889-9.1,9.957Z" transform="translate(0 -14.58)" fill="#19b399"/>
+  </svg>
+</template>
