6080763b2cc0b4189a5fcb7e5cf0cc6965443d0e
s.claes <sebastian.claes@protonmail.com>
2022-09-05 17:34:59 +0200

    [ADD] created footer template in AssetList

--- src/views/AssetList.vue
+++ src/views/AssetList.vue
@@ -17,7 +17,18 @@
       />
     </div>
 
-    <footer></footer>
+    <footer>
+      <h4>Portfoliowert</h4>
+      <span></span>
+      <span></span>
+      <span class="total-value">16.802,36 €</span>
+      <span class="total-percentage">100,00 % <IconCheck /></span>
+      <ThreeDigitValue :value-array="testDeviation" :unit="'%'" :arrow="'up'">
+        <template #arrow>
+          <IconAssetRowArrow />
+        </template>
+      </ThreeDigitValue>
+    </footer>
   </section>
 </template>
 
@@ -26,12 +37,17 @@ import SearchbarInput from '@/components/inputs/SearchbarInput.vue'
 import SearchbarContent from '@/components/wrappers/SearchbarContent.vue'
 import AssetRow from '@/components/wrappers/AssetRow.vue'
 import AssetGroup from '@/components/wrappers/AssetGroup.vue'
+import ThreeDigitValue from '@/components/data/ThreeDigitValue.vue'
+import IconAssetRowArrow from '@/assets/icons/IconAssetRowArrow.vue'
 import TableFilters from '@/components/wrappers/TableFilters.vue'
-import { computed, onMounted } from "vue";
-import AssetService from "@/services/AssetService";
-import { useAssetStore } from "@/stores/AssetStore";
+import { computed, onMounted } from 'vue';
+import AssetService from '@/services/AssetService';
+import { useAssetStore } from '@/stores/AssetStore';
+import IconCheck from '@/assets/icons/IconCheck.vue';
 
 const store = useAssetStore()
+
+let testDeviation = ['08', '62', '1']
 
 onMounted(async () => {
   store.publicAssetHoldings = await AssetService.fetchPublicAssetHoldings()
