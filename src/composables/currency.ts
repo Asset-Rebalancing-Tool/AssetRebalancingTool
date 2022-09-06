8b2831d71d43e0f8a32bc37dc6f6ae33d2b74bd9
s.claes <sebastian.claes@protonmail.com>
2022-09-06 18:19:27 +0200

    [ADD] created mapCurrencyArray function in currency composable

--- src/composables/currency.ts
+++ src/composables/currency.ts
@@ -1,6 +1,19 @@
 import type { CurrencyEnum } from '@/models/enums/CurrencyEnum'
 
 /**
+ * Map an array of currencies
+ *
+ * @param currencyArray CurrencyEnum[]
+ */
+export function mapCurrencyArray(currencyArray: CurrencyEnum[]) {
+  let tempArray = []
+  for (let currency of currencyArray) {
+    tempArray.push(mapCurrency(currency))
+  }
+  return tempArray
+}
+
+/**
  * Map the currency of an assets to its symbol
  * TODO: user configurable
  *
