a5b00b0fc8e700e5d5f1f53bcd803d3b363a1941
s.claes <sebastian.claes@protonmail.com>
2022-09-06 18:20:39 +0200

    [NEW] IconSelectArrow.vue svg component

---
+++ src/assets/icons/inputs/IconSelectArrow.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="9" height="15" viewBox="0 0 9 15"><g transform="translate(-1300 4284)"><path d="M4.5,0,9,7H0Z" transform="translate(1300 -4284)" fill="#787878"/><path d="M4.5,0,9,6H0Z" transform="translate(1309 -4269) rotate(180)" fill="#787878"/></g></svg>
+</template>
