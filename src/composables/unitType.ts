bbb2f01b2af65e1fbc6c0133797abc9341b6d248
s.claes <sebastian.claes@protonmail.com>
2022-09-06 18:24:21 +0200

    [NEW] created unitType.ts composable

---
+++ src/composables/unitType.ts
@@ -0,0 +1,41 @@
+import { UnitTypeEnum } from '@/models/enums/UnitTypeEnum';
+import type { UnitTypeEnum as UnitType } from '@/models/enums/UnitTypeEnum';
+
+/**
+ * Map an array of unit types
+ *
+ * @param unitTypeArray UnitTypeEnum[]
+ */
+export function mapUnitTypeArray(unitTypeArray: UnitType[]) {
+  let tempArray = []
+  for (let unitType of unitTypeArray) {
+    if (unitType === UnitTypeEnum.UNKNOWN) continue
+    tempArray.push(mapUnitType(unitType))
+  }
+  return tempArray
+}
+
+/**
+ * Map the asset type to its german value
+ *
+ * @param unitType UnitTypeEnum
+ */
+export function mapUnitType(unitType: UnitType): string {
+  switch (unitType) {
+    default:
+    case 'Grams':
+      return 'Gramm'
+    case 'Kilos':
+      return 'Kilo'
+    case 'Liters':
+      return 'Liter'
+    case 'Milliliters':
+      return 'Milliliter'
+    case 'Ounce':
+      return 'Unze'
+    case 'Piece':
+      return 'Stück'
+    case 'Unknown':
+      return '?'
+  }
+}
