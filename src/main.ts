ef52d80d22c0b2fd5ce7c6588e510fa8a457ea7c
s.claes <sebastian.claes@protonmail.com>
2022-09-20 13:45:45 +0200

    [ADD] vue lottie library to package json and registered it in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import './plugins/chart'
 
 import App from './App.vue'
 import router from './router'
+import Vue3Lottie from 'vue3-lottie'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vue3Lottie)
 
 app.mount('#app')
