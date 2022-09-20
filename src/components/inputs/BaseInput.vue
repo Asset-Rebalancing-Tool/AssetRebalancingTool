07e17069121d626aad3b99a5c904e9e45ad09cf0
s.claes <sebastian.claes@protonmail.com>
2022-09-20 16:48:04 +0200

    [REMOVE] unused html ref attribute

--- src/components/inputs/BaseInput.vue
+++ src/components/inputs/BaseInput.vue
@@ -3,7 +3,6 @@
   <div class="input-container" :class="customContainerClass">
     <div class="input-wrapper">
       <input
-        ref="input"
         v-bind="$attrs"
         :value="modelValue"
         :placeholder="placeholder"
