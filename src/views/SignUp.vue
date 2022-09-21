5a6d4a8b9e448329be082a6d540252e194bb43d9
admin <sebastian.claes@protonmail.com>
2022-09-21 21:02:08 +0200

    [FIX] password strength indicator

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -46,11 +46,11 @@
         <div class="error">{{ passwordError }}</div>
         <div class="form-spacing-wrapper">
           <div class="password-strength-wrapper">
-            <span :class="{ strong: passwordStrength.value >= 1 }"></span>
-            <span :class="{ strong: passwordStrength.value >= 2 }"></span>
-            <span :class="{ strong: passwordStrength.value >= 3 }"></span>
-            <span :class="{ strong: passwordStrength.value >= 4 }"></span>
-            <span :class="{ strong: passwordStrength.value >= 5 }"></span>
+            <span :class="{ strong: passwordStrength >= 1 }"></span>
+            <span :class="{ strong: passwordStrength >= 2 }"></span>
+            <span :class="{ strong: passwordStrength >= 3 }"></span>
+            <span :class="{ strong: passwordStrength >= 4 }"></span>
+            <span :class="{ strong: passwordStrength >= 5 }"></span>
           </div>
         </div>
         <button type="submit" :class="{ show: activeSubmitButton }">
@@ -152,6 +152,7 @@ const passwordStrength: Ref<number> = ref(0)
  */
 function checkPasswordStrength(passwordInput: string) {
   passwordStrength.value = requirementsFulfilled(passwordInput)
+  console.log(passwordStrength.value)
 }
 
 /**
