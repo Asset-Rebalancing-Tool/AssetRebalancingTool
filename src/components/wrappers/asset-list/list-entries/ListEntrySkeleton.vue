2005e6aa9dc590f5eff739a897a5337b4ef75a57
admin <sebastian.claes@protonmail.com>
2022-10-07 12:07:30 +0200

    [ADD] better skeleton loader animation

--- src/components/wrappers/asset-list/list-entries/ListEntrySkeleton.vue
+++ src/components/wrappers/asset-list/list-entries/ListEntrySkeleton.vue
@@ -1,5 +1,9 @@
 <template>
+
   <div class="list-entry-skeleton">
+  </div>
+
+<!--  <div class="list-entry-skeleton">
     <div class="skeleton-logo"></div>
     <div class="skeleton-info-wrapper">
       <div class="name"></div>
@@ -10,9 +14,38 @@
     <div class="skeleton-value"></div>
     <div class="skeleton-target-percentage"></div>
     <div class="skeleton-target-deviation"></div>
-  </div>
+  </div>-->
 </template>
 
 <style lang="scss">
-@import '../../../../assets/scss/wrappers/skeleton-loader';
+//@import '../../../../assets/scss/wrappers/skeleton-loader';
+
+.list-entry-skeleton {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: $inner-holding-row-height + 12px;
+  padding: 6px;
+  background-color: #292E33;
+  border-radius: $primary-border-radius;
+  margin-bottom: 15px;
+}
+
+.list-entry-skeleton:before {
+  content: '';
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, transparent, #31353f, transparent);
+  transform: translateX(-100%);
+  animation: loading 1s infinite;
+}
+
+@keyframes loading {
+  100% {
+    transform: translateX(100%);
+  }
+}
+
 </style>
