736d472592e612b40d6005f0f41d60adda523131
s.claes <sebastian.claes@protonmail.com>
2022-10-10 17:34:15 +0200

    [ADD] implemented i18n language plugin, along with translation of SignIn.vue and SignUp.vue

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import './plugins/chart'
 import App from './App.vue'
 import router from './router'
 import Vue3Lottie from 'vue3-lottie'
+import { i18n } from '@/i18n';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Vue3Lottie)
+app.use(i18n)
 
 app.mount('#app')
--- src/views/AppSettings.vue
+++ src/views/AppSettings.vue
@@ -1,5 +1,17 @@
 <template>
   <section class="content">
     <label>App Settings</label>
+    <label for="locale">Locale: </label>
+    <select
+        :value="$i18n.locale"
+        @change="setLocale($event.target.value)"
+        id="locale"
+    >
+      <option v-for="locale in allLocales" :value="locale">{{ locale }}</option>
+    </select>
   </section>
 </template>
+
+<script lang="ts" setup>
+import { allLocales, setLocale } from "@/i18n";
+</script>
