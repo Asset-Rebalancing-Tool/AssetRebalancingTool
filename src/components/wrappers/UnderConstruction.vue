9c6fafe6d23a4a1b5c292d25a266c432694f61ec
s.claes <sebastian.claes@protonmail.com>
2022-10-25 15:54:47 +0200

    [ADD] under construction text wrapper with styles and rendering

--- src/components/wrappers/UnderConstruction.vue
+++ src/components/wrappers/UnderConstruction.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="construction-container">
 
-    <div class="construction-text-wrapper" v-if="showConstructionFlag">
+    <div class="construction-text-wrapper" v-if="!showConstructionInfo">
       <h1 class>{{ $t('underConstruction.heading') }}</h1>
       <p>{{ $t('underConstruction.description') }}</p>
-      <button>{{ $t('underConstruction.moreInfo') }}</button>
+      <button @click="showConstructionInfo = true">{{ $t('underConstruction.moreInfo') }}</button>
     </div>
     <img
-      v-if="showConstructionFlag"
+      v-if="!showConstructionInfo"
       class="construction-image"
       src="@/assets/images/under-construction.png"
       :alt="$t('underConstruction.imageAltTag')"
     />
 
-    <div class="construction-info-wrapper" v-if="!showConstructionFlag">
-      <div class="close-info"></div>
-      <p>Lorem ipsum dolor</p>
+    <div class="construction-info-wrapper" v-if="showConstructionInfo">
+      <IconCloseConstructionText @click="showConstructionInfo = false" />
+      <h3>{{ constructionTitle }}</h3>
+      <p>{{ constructionText }}</p>
     </div>
   </div>
 </template>
@@ -23,15 +24,20 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import type { Ref } from "vue";
+import IconCloseConstructionText from "@/assets/icons/IconCloseConstructionText.vue";
 
 const props = defineProps({
+  constructionTitle: {
+    type: String,
+    required: true,
+  },
   constructionText: {
     type: String,
-    default: true,
-  },
+    required: true,
+  }
 })
 
-const showConstructionFlag: Ref<boolean> = ref(true)
+const showConstructionInfo: Ref<boolean> = ref(false)
 
 </script>
 
@@ -43,36 +49,66 @@ const showConstructionFlag: Ref<boolean> = ref(true)
   justify-content: center;
   align-items: center;
   padding: 25px;
-}
 
-.construction-text-wrapper {
-  margin-right: -25px;
-  max-width: 425px;
-  margin-top: 25px;
-
-  h1 {
-    font-size: 40px;
-  }
-
-  p {
-    color: $secondary-text-color;
-    font-size: 18px;
-    margin-top: 10px;
-  }
-
-  button {
-    background-color: $main-color;
-    border: none;
-    border-radius: $secondary-border-radius;
-    color: $primary-text-color;
-    padding: 10px 18px;
-    cursor: pointer;
+  .construction-text-wrapper {
+    margin-right: -25px;
+    max-width: 425px;
     margin-top: 25px;
-    font-size: 16px;
-  }
-}
 
-.construction-image {
-  margin-left: -25px;
+    h1 {
+      font-size: 40px;
+    }
+
+    p {
+      color: $secondary-text-color;
+      font-size: 18px;
+      margin-top: 10px;
+    }
+
+    button {
+      background-color: $main-color;
+      border: none;
+      border-radius: $secondary-border-radius;
+      color: $primary-text-color;
+      padding: 10px 18px;
+      cursor: pointer;
+      margin-top: 25px;
+      font-size: 16px;
+    }
+  }
+
+  .construction-image {
+    margin-left: -25px;
+  }
+
+  .construction-info-wrapper {
+    position: relative;
+    width: 500px;
+    background: $secondary-background-color;
+    border-radius: $primary-border-radius;
+    padding: 22px;
+
+    svg {
+      position: absolute;
+      top: 25px;
+      right: 25px;
+      width: 18px;
+      height: 18px;
+      cursor: pointer;
+      z-index: 25;
+    }
+
+    h3 {
+      font-size: 24px;
+      line-height: 24px;
+      margin-bottom: 18px;
+    }
+
+    p {
+      font-size: 18px;
+      color: $secondary-text-color;
+      line-height: 32px;
+    }
+  }
 }
 </style>
