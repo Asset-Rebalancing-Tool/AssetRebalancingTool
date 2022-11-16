b2afb8b1a6194c6b0ae95ebb10e1a725cb2df1e5
admin <sebastian.claes@protonmail.com>
2022-11-16 16:49:15 +0100

    [ADD] WebHistory in router index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
 import PortfolioOverview from '../views/PortfolioOverview.vue'
@@ -11,7 +11,7 @@ import { useAssetStore } from '@/stores/AssetStore'
 import { useNotificationStore } from '@/stores/NotificationStore'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/sign-in',
