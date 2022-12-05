2e13b95de433e4fb32285566057699cad836d967
s.claes <sebastian.claes@protonmail.com>
2022-12-05 13:56:13 +0100

    [ADD][WIP] BaseInput and calculation method for the PrepareInvestments.vue view

--- src/views/PrepareInvestments.vue
+++ src/views/PrepareInvestments.vue
@@ -1,5 +1,14 @@
 <template>
   <section class="content">
+
+    <BaseInput
+        type="number"
+        :modelValue="deposit"
+        :class="{ error: depositError }"
+        :select-on-focus="true"
+        @input="calculateDistribution($event.target.value)"
+    />
+
     <UnderConstruction
       :construction-title="$t('underConstruction.scheduleTab.title')"
       :construction-text="$t('underConstruction.scheduleTab.text')"
@@ -8,7 +17,23 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
 import UnderConstruction from '@/components/wrappers/UnderConstruction.vue'
 import { useI18n } from 'vue-i18n'
+import BaseInput from "@/components/inputs/BaseInput.vue";
 const { t } = useI18n()
+
+const deposit: Ref<number> = ref(0)
+const depositError: Ref<boolean> = ref(false)
+
+function calculateDistribution(inputValue: string) {
+  deposit.value = +inputValue
+  if (!depositError.value) {
+
+
+
+  }
+}
+
 </script>
